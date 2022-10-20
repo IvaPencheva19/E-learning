36fae14354922333dad5487512fe2cd228bd290f
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2022-10-20 13:55:49 +0300

    Navbar on Welcome/Login/Register pages updates based on User or no User

--- client/src/components/navbar_welcome/Navbar.jsx
+++ client/src/components/navbar_welcome/Navbar.jsx
@@ -2,30 +2,60 @@ import { Link } from "react-router-dom";
 import "./navbar.scss";
 import { theme } from "../../utils/theme";
 import { Button } from "@mui/material";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 const Navbar = () => {
   const navigate = useNavigate();
+  const { user, userLogout } = useContext(AuthContext);
+
+  const noUserButtons = () => {
+    return (
+      <>
+        <Button variant="outlined" onClick={() => navigate("/login")}>
+          Sign in
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() => navigate("/register")}
+          sx={{
+            color: "white",
+          }}
+        >
+          Sign up
+        </Button>
+      </>
+    );
+  }
+
+  const userButtons = () => {
+    return (
+      <>
+        <Button variant="contained"
+          sx={{
+            color: "white",
+          }}
+          onClick={userLogout}>
+          Logout
+        </Button>
+      </>
+    );
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <div className="navWrapper">
-        <Link to="/">
+        <Link to="/" >
           <img className="navbarLogo" src="./img/connect.webp" alt="" />
         </Link>
 
         <div className="navButtonsContainer">
-          <Button variant="outlined" onClick={() => navigate("/login")}>
-            Sign in
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => navigate("/register")}
-            sx={{
-              color: "white",
-            }}
-          >
-            Sign up
-          </Button>
+          {user.email
+            ? userButtons()
+            : noUserButtons()
+          }
         </div>
       </div>
     </ThemeProvider>
