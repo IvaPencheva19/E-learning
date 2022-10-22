a141a3df3386bed7a32ba8667633845c02396a92
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2022-10-22 06:10:19 +0300

    Meaningful comments to App.js added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 
-import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Welcome from "./pages/welcome/Welcome";
@@ -10,9 +9,9 @@ import HomeTeacher from "./pages/home_teacher/Home";
 import AllCourses from "./pages/view_all_courses_student/AllCourses";
 import AllCoursesTeacher from "./pages/view_all_courses_teacher/AllCoursesTeacher";
 import AddCourse from "./pages/add_new_course/AddCourse";
+import { AuthProvider } from "./context/AuthContext";
 import { PrivateRoute } from "./components/common/PrivateRoute";
 import { PublicRoute } from "./components/common/PublicRoute";
-
 
 
 function App() {
@@ -21,14 +20,17 @@ function App() {
       <main id="main-content">
         <AuthProvider >
           <Routes>
+            {/* accessed by everyone routes */}
+            <Route path="/" element={<Welcome />} />
 
             <Route element={<PublicRoute />}>
+              {/* public routes */}
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              <Route path="/" element={<Welcome />} />
             </Route>
 
             <Route element={<PrivateRoute />}>
+              {/* private routes */}
               <Route path="/home" element={<Home />} />
               <Route path="/homeTeacher" element={<HomeTeacher />} />
               <Route path="/viewAllCourses" element={<AllCourses />} />
