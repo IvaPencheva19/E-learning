0e8401835cf95021d93217b4de131b42f0b098d6
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2022-10-23 16:59:59 +0300

    profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -2,10 +2,13 @@ import "./profile.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
-import List from "../../components/table/Table";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Single = () => {
+  const { user } = useContext(AuthContext);
   return (
+
     <div className="single">
       <Sidebar />
       <div className="singleContainer">
@@ -16,29 +19,25 @@ const Single = () => {
             <h1 className="title">Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                src={user.imageUrl}
                 alt=""
                 className="itemImg"
               />
               <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
+                <h1 className="itemTitle">{user.firstName} {user.lastName}</h1>
                 <div className="detailItem">
                   <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemValue">{user.email}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Phone:</span>
-                  <span className="itemValue">+1 2345 67 89</span>
+                  <span className="itemKey">Username:</span>
+                  <span className="itemValue">{user.username}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Address:</span>
+                  <span className="itemKey">Role:</span>
                   <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
+                    {user.role}
                   </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Country:</span>
-                  <span className="itemValue">USA</span>
                 </div>
               </div>
             </div>
@@ -46,10 +45,6 @@ const Single = () => {
           <div className="right">
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
           </div>
-        </div>
-        <div className="bottom">
-          <h1 className="title">Last Transactions</h1>
-          <List />
         </div>
       </div>
     </div>
