66469ff463133861eb277875250f0accc39f82e9
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2022-11-18 12:54:46 +0200

    Fixed bug with expired token

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,9 +1,17 @@
 import { useState } from "react"
 
 export const useLocalStorage = (key, defaultValue) => {
-    
+
     const [value, setValue] = useState(() => {
         const storedData = localStorage.getItem(key);
+
+
+        const objStoredData = JSON.parse(storedData);
+        const currentDate = new Date();
+        if (objStoredData?.exp * 1000 <= currentDate.getTime()) {
+            localStorage.setItem(key, JSON.stringify({}));
+            return defaultValue;
+        }
 
         return storedData ? JSON.parse(storedData) : defaultValue;
     });
